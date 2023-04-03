fd1a16d8d396d2b2c2415709eb6dabc82b622ba9
VladislavCheremnov <88377818+VladislavCheremnov@users.noreply.github.com>
2023-04-03 13:43:46 +0700

    Перебил на компоненты: главное меню и логотип.

---
+++ include/logo.php
@@ -0,0 +1,3 @@
+<div class="logo">
+ <a href="/"><img width="164" alt="logo.png" src="/upload/medialibrary/d8e/okgv5dkgk2fyux89qwd5etq2ywwkv8au.png" height="51" title="logo.png" class="img-responsive"></a>
+</div>
